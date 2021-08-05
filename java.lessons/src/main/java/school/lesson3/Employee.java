4ef5ec972fa7a1dee0dffa1c12df3f3b607e0c90
Sasha <sashel81@gmail.com>
2021-08-05 17:12:13 +0300

    Lesson_3

--- java.lessons/src/main/java/school/lesson3/Employee.java
+++ java.lessons/src/main/java/school/lesson3/Employee.java
@@ -19,10 +19,51 @@ package school.lesson3;
         public int getAge () {
             return age;
         }
+     public void setAge (int Age) {
+         this.age = age;
+     }
         public void info () {
             System.out.print(name + " " + position + " " + email + " " + phone + " " + salary + "" + age + "\n");
         }
 
+     public String getName() {
+         return name;
+     }
 
-    }
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     public String getPosition() {
+         return position;
+     }
+
+     public void setPosition(String position) {
+         this.position = position;
+     }
+
+     public String getEmail() {
+         return email;
+     }
+
+     public void setEmail(String email) {
+         this.email = email;
+     }
+
+     public String getPhone() {
+         return phone;
+     }
+
+     public void setPhone(String phone) {
+         this.phone = phone;
+     }
+
+     public int getSalary() {
+         return salary;
+     }
+
+     public void setSalary(int salary) {
+         this.salary = salary;
+     }
+ }
 
